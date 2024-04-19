291b7f710ccb47c9103c5c9bbdb2f7f05de74aa4
etornam19 <eadevor@hawk.iit.edu>
2024-04-19 03:51:49 -0500

    updated map and jscript file

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function initMap(){
+    map = new google.map.Map(document.getElementById("map"))
+    //add your javascrip between these two lines of code
+    var boba = {lat: 40.700802, lng:73.987602};
+    var googlemap = new google.maps.Map(document.getElementById("map"), {center: boba});
+    var loc = new google.maps.Marker({position: boba, map: googlemap})
+
+}
+google.maps.event.addDomListener(window,'load', init);
