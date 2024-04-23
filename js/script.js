6df3519d718463dd6d5af209d0d40202b360c7f2
etornam19 <eadevor@hawk.iit.edu>
2024-04-23 16:41:12 -0500

    updated js file

--- js/script.js
+++ js/script.js
@@ -1,10 +1,12 @@
-/*function initMap(){
-    map = new google.map.Map(document.getElementById("map"))
-    //add your javascrip between these two lines of code
-    let boba = {lat: 41.85441589355469, lng:-87.63263702392578};
-    let map = new google.map.Map(
-        document.getElementById("map"), {zoom:10, center: boba});
-    let loc = new google.maps.Marker({position: boba, map: googlemap})
+function displayMap(){
+  const mapOptions = {
+    center: {lat: 41.85441589355469, lng:-87.63263702392578},
+    zoom: 10
+  };
+    
+  const mapDiv  = document.getElementById('map');
+  const map = new google.map.Map( mapDiv, mapOptions);
+  return map;
 
 }
-google.maps.event.addDomListener(window,'load', initMap);*/
+google.maps.event.addDomListener(window,'load', initMap);
