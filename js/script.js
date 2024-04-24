882d7b7b6b06138477870395b6813a6c9a466052
etornam19 <eadevor@hawk.iit.edu>
2024-04-24 15:54:02 -0500

    updated map

--- js/script.js
+++ js/script.js
@@ -1,17 +1,17 @@
 let map;
 async function initMap(){
     //add your javascrip between these two lines of code
-    let boba = {lat: 41.85441589355469, lng:-87.63263702392578};
+    const boba = {lat: 41.85441589355469, lng:-87.63263702392578};
     const { Map } = await google.maps.importLibrary("maps");
     const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
     
-    let map = new google.map.Map(document.getElementById("map"), {
+    map = new google.map.Map(document.getElementById("map"), {
         zoom:10, 
         center: boba
     });
-    let loc = new google.maps.Marker({
+    marker = new AdvancedMarkerElement({
         position: boba, 
-        map: map
+        map: map,
     });
 }
 initMap();
