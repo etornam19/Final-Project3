81803a8e73f94fda5f171044f7d3b739331b98eb
etornam19 <eadevor@hawk.iit.edu>
2024-04-24 19:15:18 -0500

    updated script

--- js/script.js
+++ js/script.js
@@ -1,17 +1,27 @@
+// Initialize and add the map
 let map;
-async function initMap(){
-    //add your javascrip between these two lines of code
-    const boba = {lat: 41.85441589355469, lng:-87.63263702392578};
-    const { Map } = await google.maps.importLibrary("maps");
-    const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
-    
-    map = new google.map.Map(document.getElementById("map"), {
-        zoom:10, 
-        center: boba
-    });
-    marker = new AdvancedMarkerElement({
-        position: boba, 
-        map: map,
-    });
+
+async function initMap() {
+  // The location of Uluru
+  const position = {lat: 41.85441589355469, lng:-87.63263702392578};
+  // Request needed libraries.
+  //@ts-ignore
+  const { Map } = await google.maps.importLibrary("maps");
+  const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
+
+  // The map, centered at Uluru
+  map = new Map(document.getElementById('boba'), {
+    zoom: 14,
+    center: position,
+    mapId: "DEMO_MAP_ID",
+  });
+
+  // The marker, positioned at Uluru
+  const marker = new AdvancedMarkerElement({
+    map: map,
+    position: position,
+    title: "Boba",
+  });
 }
-google.maps.event.addDomListener(window, 'load', initMap);
+
+initMap();
