7386418f7623c2699f4c705f9b6d8fd264ef27d4
etornam19 <eadevor@hawk.iit.edu>
2024-04-25 15:11:10 -0500

    renamed marker

--- js/map.js
+++ js/map.js
@@ -2,25 +2,23 @@
 let map;
 
 async function initMap() {
-  // The location of Uluru
-  const position = {lat: 41.85441589355469, lng:-87.63263702392578};
-  // Request needed libraries.
-  //@ts-ignore
+  
+  var ma = document.getElementById('boba');
+  var position = {lat: 41.854415, lng:-87.632637};
+
   const { Map } = await google.maps.importLibrary("maps");
   const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
 
-  // The map, centered at Uluru
   var map = new Map(document.getElementById('boba'), {
     zoom: 14,
     center: position,
     mapId: "DEMO_MAP_ID",
   });
 
-  // The marker, positioned at Uluru
   const marker = new AdvancedMarkerElement({
     map: map,
     position: position,
-    title: "Boba",
+    title: "Tiger Sugar Boba Shop",
   });
 }
 
